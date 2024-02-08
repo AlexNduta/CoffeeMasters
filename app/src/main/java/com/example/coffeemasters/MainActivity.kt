dd9cfe5e13c03308a4d717eab91b8ae4947d225e
Alex Nduta <wasremoved@gmail.com>
2024-02-08 18:02:46 +0300

    Cofee Masters setup

---
+++ app/src/main/java/com/example/coffeemasters/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.coffeemasters
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.Ro
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coffeemasters.ui.theme.CoffeeMastersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoffeeMastersTheme {
+                // A surface container using the 'FirstComposable()background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    messageCard(msg = Message("Alex", "Wutang"))
+
+                }
+            }
+        }
+    }
+}
+
+
+data class Message(val author:String, val body:String)
+@Composable
+fun messageCard(msg:Message){
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
+}
+
+
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    messageCard(
+        msg = Message("Alex", "Take a look at Jetpack COmpose")
+    )
+}
